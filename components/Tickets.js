5650ef04f430ab375454a6631a97760f78effc9a
Michael <mich4s74@stud.kea.dk>
2023-05-25 20:08:49 +0200

    adjusted checkout

--- components/Tickets.js
+++ components/Tickets.js
@@ -63,14 +63,14 @@ const Tickets = () => {
           <p>Tickets</p>
           <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
             <img
-              src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              src="./assets/icon/regular_ticket.png"
               alt="product-image"
-              class="w-full rounded-lg sm:w-40"
+              class="w-full rounded-lg sm:w-32"
             />
             <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
               <div class="mt-5 sm:mt-0">
                 <h2 class="text-lg font-bold text-gray-900">Regular Ticket</h2>
-                <p class="mt-1 text-xs text-gray-700">Price: 799</p>
+                <p class="mt-1 text-xs text-gray-700">Description here...</p>
               </div>
               <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                 <div class="flex items-center border-gray-100">
@@ -96,35 +96,22 @@ const Tickets = () => {
                 </div>
                 <div class="flex items-center space-x-4">
                   <p class="text-sm">
-                    {regularTicketPrice * regularTicketQuantity},-
+                    Price: {regularTicketPrice * regularTicketQuantity},-
                   </p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
                 </div>
               </div>
             </div>
           </div>
           <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
             <img
-              src="https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80"
+              src="./assets/icon/vip_ticket.png"
               alt="product-image"
-              class="w-full rounded-lg sm:w-40"
+              class="w-full rounded-lg sm:w-32"
             />
             <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
               <div class="mt-5 sm:mt-0">
                 <h2 class="text-lg font-bold text-gray-900">VIP ticket</h2>
-                <p class="mt-1 text-xs text-gray-700">Price: 1299</p>
+                <p class="mt-1 text-xs text-gray-700">Description here...</p>
               </div>
               <div class="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                 <div class="flex items-center border-gray-100">
@@ -149,20 +136,9 @@ const Tickets = () => {
                   </span>
                 </div>
                 <div class="flex items-center space-x-4">
-                  <p class="text-sm">{vipTicketPrice * vipTicketQuantity},-</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
+                  <p class="text-sm">
+                    Price: {vipTicketPrice * vipTicketQuantity},-
+                  </p>
                 </div>
               </div>
             </div>
@@ -176,8 +152,10 @@ const Tickets = () => {
             />
             <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
               <div class="mt-5 sm:mt-0">
-                <h2 class="text-lg font-bold text-gray-900">VIP ticket</h2>
-                <p class="mt-1 text-xs text-gray-700">Price: 1299</p>
+                <h2 class="text-lg font-bold text-gray-900">
+                  2 person tent (including the tent)
+                </h2>
+                <p class="mt-1 text-xs text-gray-700">Description here...</p>
               </div>
               <div class="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                 <div class="flex items-center border-gray-100">
@@ -198,20 +176,7 @@ const Tickets = () => {
                   </span>
                 </div>
                 <div class="flex items-center space-x-4">
-                  <p class="text-sm">1299,-</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
+                  <p class="text-sm">Price: 299 ,-</p>
                 </div>
               </div>
             </div>
@@ -224,31 +189,56 @@ const Tickets = () => {
             />
             <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
               <div class="mt-5 sm:mt-0">
+                <h2 class="text-lg font-bold text-gray-900">
+                  3 person tent (including the tent)
+                </h2>
+                <p class="mt-1 text-xs text-gray-700">Description here...</p>
+              </div>
+              <div class="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                <div class="flex items-center border-gray-100">
+                  <span class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                    {" "}
+                    -{" "}
+                  </span>
+                  <input
+                    class="h-8 w-8 border bg-white text-center text-xs outline-none"
+                    type="number"
+                    value="2"
+                    min="1"
+                    readOnly
+                  />
+                  <span class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                    {" "}
+                    +{" "}
+                  </span>
+                </div>
+                <div class="flex items-center space-x-4">
+                  <p class="text-sm">Price: 399 ,-</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+            <img
+              src="./assets/icon/green-love.png"
+              alt="product-image"
+              class="w-full rounded-lg sm:w-32"
+            />
+            <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+              <div class="mt-5 sm:mt-0">
                 <h2 class="text-lg font-bold text-gray-900">Green Fee</h2>
-                <p class="mt-1 text-xs text-gray-700">Price: 249</p>
+                <p class="mt-1 text-xs text-gray-700">Description here...</p>
               </div>
               <div class="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                 <div class="flex items-center border-gray-100">
                   <input
                     checked={isChecked}
                     onChange={handleCheckboxChange}
-                    type="checkbox"></input>
+                    type="checkbox"
+                    className="w-6 h-6"></input>
                 </div>
                 <div class="flex items-center space-x-4">
                   <p class="text-sm">249 ,-</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
                 </div>
               </div>
             </div>
@@ -276,9 +266,11 @@ const Tickets = () => {
               <p class="text-sm text-gray-700">including VAT</p>
             </div>
           </div>
-          <ButtonPrimary onClick={() => router.push("/checkout")}>
-            Check out
-          </ButtonPrimary>
+          <div className="m-8">
+            <ButtonPrimary onClick={() => router.push("/checkout")}>
+              Check out
+            </ButtonPrimary>
+          </div>
         </div>
       </div>
     </div>
