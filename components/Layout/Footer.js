dfd8fb8d2fe018b51122b6c334177ebae64f2683
Mokkel <qaikocss@homtmail.dk>
2023-05-31 13:53:02 +0200

    link from logo to frontpage

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="bg-white-300 pt-64 pb-24 ">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          <a href="#" title="">
+          <a href="/" title="">
             {" "}
             <Logo className="h-16 w-auto mb-6" />
           </a>
